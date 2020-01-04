ec9d4e0d24e70aae832d1e45fd77972090bd86f9
zhoubin <zoe27@126.com>
2020-01-04 17:23:49 +0800

    1、乱码检索过滤

--- site-page/src/main/java/cn/com/site/page/service/impl/SearchSiteServiceImpl.java
+++ site-page/src/main/java/cn/com/site/page/service/impl/SearchSiteServiceImpl.java
@@ -19,41 +19,44 @@ import cn.com.site.es.esUtil.ESClient;
 import cn.com.site.es.util.ESConstant;
 import cn.com.site.page.dto.SiteResDto;
 import cn.com.site.page.service.SearchService;
+import cn.com.site.page.util.MessCodeUtil;
 
 /**
  * @Description: TODO
  * @author 作者 zhoubin
- * @version 创建时间：2019年8月4日 上午10:33:32
- * SearchSiteServiceImpl.java
+ * @version 创建时间：2019年8月4日 上午10:33:32 SearchSiteServiceImpl.java
  * @since JDK 1.8
- * @version V1.0
- * Copyright (c) 2019, zoe27@126.com All Rights Reserved.
+ * @version V1.0 Copyright (c) 2019, zoe27@126.com All Rights Reserved.
  * 
  */
 @Service
 public class SearchSiteServiceImpl implements SearchService {
-	
+
 	@Autowired
 	private ESClient esclient;
-	
+
 	private Logger log = LoggerFactory.getLogger(this.getClass());
 
-	/* (non-Javadoc)
-	 * @see cn.com.site.page.service.SearchService#searchByQuery(java.lang.String, int, int)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see cn.com.site.page.service.SearchService#searchByQuery(java.lang.String,
+	 * int, int)
 	 */
 	@Override
 	public List<SiteResDto> searchByQuery(String query, int begin, int size) {
 		List<SiteResDto> list = Lists.newArrayList();
 		Set<String> titleSet = Sets.newHashSet();
 		// TODO Auto-generated method stub
-		SearchResponse response = esclient.getData(begin, size, ESConstant.ES_SITE_INFO.INDEX.getInfo(),ESConstant.ES_SITE_INFO.TYPE.getInfo(),query);
+		SearchResponse response = esclient.getData(begin, size, ESConstant.ES_SITE_INFO.INDEX.getInfo(),
+				ESConstant.ES_SITE_INFO.TYPE.getInfo(), query);
 		SearchHits hits = null;
 		long time = System.currentTimeMillis();
-		if(response != null) {
+		if (response != null) {
 			hits = response.getHits();
 		}
-		if(hits != null) {
-			hits.forEach(hit->{
+		if (hits != null) {
+			hits.forEach(hit -> {
 				String title = hit.getSourceAsMap().get(ESConstant.TITLE).toString();
 				String url = hit.getSourceAsMap().get(ESConstant.URL).toString();
 				// 去除重复
@@ -68,11 +71,18 @@ public class SearchSiteServiceImpl implements SearchService {
 				String imagePath = hit.getSourceAsMap().get(ESConstant.IMAGE_PATH).toString();
 				String desc = hit.getSourceAsMap().get(ESConstant.DESC).toString();
 
-				if(StringUtils.isNotBlank(desc)) {
+				if (StringUtils.isNotBlank(desc)) {
 					keywords = desc;
 				}
+				
+				// 乱码过滤
+				if (MessCodeUtil.isMessyCode(title) || MessCodeUtil.isMessyCode(desc)) {
+					log.warn("mess info: title is {}, desc is {}", title, desc);
+					return;
+				}
+				
 				// 高亮命中
-				if(null != hit.getHighlightFields().get(ESConstant.TITLE)) {
+				if (null != hit.getHighlightFields().get(ESConstant.TITLE)) {
 					title = hit.getHighlightFields().get(ESConstant.TITLE).getFragments()[0].toString();
 				}
 				SiteResDto siteDto = new SiteResDto(title, url, imagePath, keywords);
@@ -83,26 +93,29 @@ public class SearchSiteServiceImpl implements SearchService {
 		return list;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see cn.com.site.page.service.SearchService#searchRecent()
 	 */
 	@Override
 	public List<SiteResDto> searchRecent() {
 		List<SiteResDto> list = Lists.newArrayList();
 		// TODO Auto-generated method stub
-		SearchResponse response = esclient.getRecentData(ESConstant.ES_SITE_INFO.INDEX.getInfo(),ESConstant.ES_SITE_INFO.TYPE.getInfo());
+		SearchResponse response = esclient.getRecentData(ESConstant.ES_SITE_INFO.INDEX.getInfo(),
+				ESConstant.ES_SITE_INFO.TYPE.getInfo());
 		SearchHits hits = null;
 		long time = System.currentTimeMillis();
-		if(response != null) {
+		if (response != null) {
 			hits = response.getHits();
 		}
-		if(hits != null) {
-			for(int i = 0; i < hits.getHits().length; i++){
+		if (hits != null) {
+			for (int i = 0; i < hits.getHits().length; i++) {
 				SearchHit hit = hits.getHits()[i];
 				String title = hit.getSourceAsMap().get(ESConstant.TITLE).toString();
 				// title 长度控制在15个字符左右
-				if(StringUtils.isNotBlank(title) && title.length() > 15) {
-					title = title.substring(0,15).concat("....");
+				if (StringUtils.isNotBlank(title) && title.length() > 15) {
+					title = title.substring(0, 15).concat("....");
 				}
 				String url = hit.getSourceAsMap().get(ESConstant.URL).toString();
 				// 这里是为了防止前端按钮出不来的问题
@@ -113,12 +126,19 @@ public class SearchSiteServiceImpl implements SearchService {
 				String imagePath = hit.getSourceAsMap().get(ESConstant.IMAGE_PATH).toString();
 				String desc = hit.getSourceAsMap().get(ESConstant.DESC).toString();
 
-				if(StringUtils.isNotBlank(desc)) {
+				// 乱码过滤
+				if (MessCodeUtil.isMessyCode(title) || MessCodeUtil.isMessyCode(desc)) {
+					log.warn("mess info: title is {}, desc is {}", title, desc);
+					continue;
+				}
+
+				if (StringUtils.isNotBlank(desc)) {
 					keywords = desc;
 				}
 				SiteResDto siteDto = new SiteResDto(title, url, imagePath, keywords);
 				list.add(siteDto);
-			};
+			}
+			;
 		}
 		log.info("get recent data from es cost time is {}, query is {}", (System.currentTimeMillis() - time));
 		list = list.size() > 10 ? list.subList(0, 10) : list;
@@ -126,4 +146,3 @@ public class SearchSiteServiceImpl implements SearchService {
 	}
 
 }
-
